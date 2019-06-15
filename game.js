c6d2cdb1797aa9fdfe14f5321abb852d6b3b1239
smhailey <smhailey@msn.com>
2019-06-14 23:10:21 -0600

    Stuck on addMods function

--- game.js
+++ game.js
@@ -1,30 +1,88 @@
-let playerName = 'Herbert'
-let playerHealth = 100;
-let numberOfHits = 0;
-let healthStatus = document.querySelector("#health")
-let hitsStatus = document.querySelector("#hits")
+let playerName = 'Herbert';
+let healthStatus = document.querySelector("#health");
+let hitStatus = document.querySelector("#hits");
+
+let target = {
+  playerHealth: 100,
+  numberOfHits: 0,
+  attacks: {
+    slap: 1,
+    punch: 5,
+    kick: 10
+  },
+  items: []
+}
+
+let items = {
+  shield: { name: 'Shield', modifier: .2, description: 'It protects!' },
+  bat: { name: 'Bat', modifier: 2, description: 'Bonk!' },
+  fire: { name: 'Fire', modifier: 3, description: 'IT BURNS!' }
+}
 
 function slap() {
-  playerHealth--
-  numberOfHits++
+  target.playerHealth -= Math.trunc(1 * addMods())
+  target.numberOfHits++
   update()
+  return;
 }
 
 function punch() {
-  playerHealth -= 5
-  numberOfHits++
+  target.playerHealth -= Math.trunc(5 * addMods())
+  target.numberOfHits++
   update()
+  return;
 }
 
 function kick() {
-  playerHealth -= 10
-  numberOfHits++
+  target.playerHealth -= Math.trunc(10 * addMods())
+  target.numberOfHits++
   update()
+  return;
+}
+
+// function giveItem(item) {
+
+// }
+
+function giveShield() {
+  target.items.push(items.shield)
+  return;
+}
+
+function giveBat() {
+  target.items.push(items.bat)
+  return;
+}
+
+function giveFire() {
+  target.items.push(items.fire)
+  return;
+}
+
+function addMods() {
+  let modifier = 0;
+  for (let i = 0; i < target.items.length; i++) {
+    if (target.items[i].name == 'Fire') {
+      modifier += target.items[i].modifier;
+    }
+    if (target.items[i].name == 'Bat') {
+      modifier += target.items[i].modifier;
+    }
+    if (modifier == 0) {
+      ++modifier;
+    }
+    if (target.items[i].name == 'Shield') {
+      modifier *= 0.2;
+    }
+  }
+  debugger
+  return modifier;
 }
 
 function update() {
-  healthStatus.innerHTML = playerHealth.toString()
-  hitsStatus.innerHTML = numberOfHits.toString()
+  healthStatus.innerHTML = target.playerHealth.toString()
+  hitStatus.innerHTML = target.numberOfHits.toString()
+  return;
 }
 
 update()
